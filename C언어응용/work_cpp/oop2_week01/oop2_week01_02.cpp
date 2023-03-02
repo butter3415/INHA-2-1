8fbf9e9bb7b116ff64fa5a02ed4ef1ecfb710a33
butter3415 <butter3415@gmail.com>
2023-03-02 22:24:45 +0900

    C언어 응용 수업 (1주차-복습)

--- C언어응용/work_cpp/oop2_week01/oop2_week01_02.cpp
+++ C언어응용/work_cpp/oop2_week01/oop2_week01_02.cpp
@@ -1,33 +1,33 @@
-#include <iostream>	
-using namespace std;	
-
-
-int main() { 
-	int number;
-	bool isPrime = true;	// 약수 발생 시 false
-
-	cin >> number;	// Scanner sc = new Scanner(System.in); int number = sc.nextInt();
-	
-	// cout << number << endl;
-
-	if (number < 1) {
-		isPrime = false;
-	}
-	else {
-		for (int i = 2; i < number; i++) {
-			if (number % i == 0) {
-				isPrime = false;
-				break;	// for구문 탈출
-			}
-		}
-	}
-
-	if (isPrime == true) {
-		cout << number << " is prime number." << endl;	// System.out.println("~~~");
-	}
-	else {
-		cout << number << " is NOT prime number." << endl;
-	}
-
-	// return 0;
-}
+//#include <iostream>	
+//using namespace std;	
+//
+//
+//int main() { 
+//	int number;
+//	bool isprime = true;	// 약수 발생 시 false
+//
+//	cin >> number;	// scanner sc = new scanner(system.in); int number = sc.nextint();
+//	
+//	// cout << number << endl;
+//
+//	if (number < 1) {
+//		isprime = false;
+//	}
+//	else {
+//		for (int i = 2; i < number; i++) {
+//			if (number % i == 0) {
+//				isprime = false;
+//				break;	// for구문 탈출
+//			}
+//		}
+//	}
+//
+//	if (isprime == true) {
+//		cout << number << " is prime number." << endl;	// system.out.println("~~~");
+//	}
+//	else {
+//		cout << number << " is not prime number." << endl;
+//	}
+//
+//	// return 0;
+//}
---
+++ C언어응용/work_cpp/oop2_week01/oop2_week01_review.cpp
@@ -0,0 +1,33 @@
+# include<iostream>
+using namespace std;
+
+int main() {
+	int number;
+	bool isPrime = true;
+
+	cout << "소수 판정 수 :";
+	cin >> number;
+
+	if (number < 1) {
+		isPrime = false;
+	}
+	else {
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+	}
+
+	if (isPrime) {
+		cout << number << " is Prime Number." << endl;
+	}
+	else {
+		cout << number << " is NOT Prime Number." << endl;
+	}
+
+
+
+}
