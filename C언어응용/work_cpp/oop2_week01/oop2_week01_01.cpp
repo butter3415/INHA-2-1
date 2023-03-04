d5c0c143b04a2657a7ac8221f9f474ac00ba72b2
butter3415 <butter3415@gmail.com>
2023-03-04 17:21:26 +0900

    C언어 응용 수업 (1주차-오프라인)

--- C언어응용/work_cpp/oop2_week01/oop2_week01_01.cpp
+++ C언어응용/work_cpp/oop2_week01/oop2_week01_01.cpp
@@ -2,7 +2,7 @@
 // Ctrl + K + C, Ctrl + K + U
 
 //#include <iostream>	// input output stream 약자 -> 출력객체
-//using namespace std;	// std standards for Standard namespace
+//using namespace std;	// std standards for Standard namespace // 패키지 같은 느낌 // 폴더 안 만들어도 사용 가능. 포함 라이브러리
 //
 //
 //int main() {	// program entry point 
